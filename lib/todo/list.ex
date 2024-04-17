0c143c4c773854b8d75a91e240421f9975a46ebb
edmtsky <161596840+edmtsky@users.noreply.github.com>
2024-04-17 13:45:38 +0300

    add tests for Todo.List

--- lib/todo/list.ex
+++ lib/todo/list.ex
@@ -5,6 +5,10 @@ defmodule Todo.List do
     Enum.reduce(entries, %Todo.List{}, &add_entry(&2, &1))
   end
 
+  def size(todo_list) do
+    map_size(todo_list.entries)
+  end
+
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.next_id)
 
