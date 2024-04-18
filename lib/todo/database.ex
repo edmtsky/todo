a0973fb207ff83ae73e1448e61f084823f7cc3ce
edmtsky <161596840+edmtsky@users.noreply.github.com>
2024-04-18 11:41:36 +0300

    cleanup Todo.Datebase

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -22,9 +22,11 @@ defmodule Todo.Database do
     Todo.DatabaseWorker.get(worker, key)
   end
 
-  # Selecting a worker makes a request to the database server process. There we
-  # keep the knowledge about our workers, and return the pid of the corresponding
-  # worker. Once this is done, the caller process will talk to the worker directly.
+  # Selecting a worker through a request to the database server process.
+  # Information about running workers is stored within the database process state.
+  # The purpose of this function is to return the pid of one worker from a pool.
+  # The worker is selected based on a specific key - the name of the to-do list.
+  # Next, the caller process will talk to the worker directly.
   defp_testable select_worker(key) do
     GenServer.call(__MODULE__, {:select_worker, key})
   end
@@ -37,35 +39,11 @@ defmodule Todo.Database do
     {:ok, start_workers()}
   end
 
-  @impl GenServer
-  def handle_cast({:store, key, data}, state) do
-    key
-    |> file_name()
-    |> File.write!(:erlang.term_to_binary(data))
-
-    {:noreply, state}
-  end
-
-  # for testing
-  @impl GenServer
-  def handle_cast({:stop_db, reason}, state) do
-    state
-    |> Enum.each(fn {_idx, worker_pid} ->
-      GenServer.stop(worker_pid, reason)
-    end)
-
-    {:stop, reason, state}
-  end
-
-  @impl GenServer
-  def handle_call({:get, key}, _, state) do
-    data =
-      case File.read(file_name(key)) do
-        {:ok, contents} -> :erlang.binary_to_term(contents)
-        _ -> nil
-      end
-
-    {:reply, data, state}
+  defp start_workers() do
+    for index <- 1..3, into: %{} do
+      {:ok, worker_pid} = Todo.DatabaseWorker.start(@db_directory)
+      {index - 1, worker_pid}
+    end
   end
 
   @impl GenServer
@@ -75,35 +53,23 @@ defmodule Todo.Database do
     {:reply, worker_pid, state}
   end
 
-  # for testing
+  # --------------------------------------------------------------------------
+  #                            testing helpers
+
   @impl GenServer
   def handle_call({:workers}, _, state) do
     {:reply, state, state}
   end
 
-  # for testing
   @impl GenServer
-  def handle_call({:stop_worker, idx, reason}, _from, state) do
-    case Map.fetch(state, idx) do
-      {:ok, worker_pid} ->
-        GenServer.stop(worker_pid, reason)
-        new_state = Map.delete(state, idx)
-        {:reply, :ok, new_state}
+  def handle_cast({:stop_db, reason}, state) do
+    state
+    |> Enum.each(fn {_idx, worker_pid} ->
+      GenServer.stop(worker_pid, reason)
+    end)
 
-      :error ->
-        {:reply, :not_found, state}
-    end
+    {:stop, reason, state}
   end
-
-  defp start_workers() do
-    for index <- 1..3, into: %{} do
-      {:ok, worker_pid} = Todo.DatabaseWorker.start(@db_directory)
-      {index - 1, worker_pid}
-    end
-  end
-
-  # --------------------------------------------------------------------------
-  #                            testing helpers
 
   @doc """
   remove all files from persist directory "ceanup all db-records"
@@ -120,15 +86,6 @@ defmodule Todo.Database do
   """
   defp_testable workers() do
     GenServer.call(__MODULE__, {:workers})
-  end
-
-  # def testing_select_worker(key) do select_worker(key) end
-
-  @doc """
-  for testing - stop worker by its index in map(0-2)
-  """
-  defp_testable stop_worker(idx, reason \\ :normal) do
-    GenServer.cast(__MODULE__, {:stop_worker, idx, reason})
   end
 
   @doc """
