9a8e7c16a89e2df6b8a5e6cd07eaabefb3d90c8e
edmtsky <skythoren@gmail.com>
2024-04-20 20:26:18 +0300

    9.2 dynamic supervisor for Todo.Server processes
    - rewrite Todo.Cache as DynamicSupervisor
    - use Todo.Server as :temporary childs of Todo.Cache Supervisor
    - remove unused test helpers

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -1,69 +1,45 @@
 defmodule Todo.Cache do
-  use GenServer
   use Todo.Utils
 
   # Client API
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+
+  def start_link() do
+    dputs("Starting to-do cache")
+
+    DynamicSupervisor.start_link(
+      name: __MODULE__,
+      strategy: :one_for_one
+    )
   end
 
   # return pid of Todo.Server
   @spec server_process(todo_list_name :: String.t()) :: pid
   def server_process(todo_list_name) do
-    GenServer.call(__MODULE__, {:server_process, todo_list_name})
-  end
-
-  # Server callbacks
-
-  @impl GenServer
-  def init(_) do
-    dputs("Starting to-do cache.")
-    {:ok, %{}}
-  end
-
-  @impl GenServer
-  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
-    case Map.fetch(todo_servers, todo_list_name) do
-      {:ok, todo_server} ->
-        {:reply, todo_server, todo_servers}
-
-      :error ->
-        {:ok, new_server} = Todo.Server.start_link(todo_list_name)
-
-        {
-          :reply,
-          new_server,
-          Map.put(todo_servers, todo_list_name, new_server)
-        }
+    case start_child(todo_list_name) do
+      {:ok, pid} -> pid
+      {:error, {:already_started, pid}} -> pid
     end
+  end
+
+  # this code will trigger Todo.Server.start_link(todo_list_name)
+  defp start_child(todo_list_name) do
+    DynamicSupervisor.start_child(
+      __MODULE__,
+      {Todo.Server, todo_list_name}
+    )
+  end
+
+  # used by Todo.System supervisor to run this Todo.Cache Supervisor as child
+  def child_spec(_arg) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []},
+      type: :supervisor
+    }
   end
 
   # --------------------------------------------------------------------------
   #                            testing helpers
-
-  @impl GenServer
-  def handle_call({:close_process, todo_list_name}, _, todo_servers) do
-    case Map.fetch(todo_servers, todo_list_name) do
-      {:ok, todo_server} ->
-        GenServer.stop(todo_server, :normal)
-        new_todo_servers = Map.delete(todo_servers, todo_list_name)
-        {:reply, todo_server, new_todo_servers}
-
-      :error ->
-        {:reply, :not_found, todo_servers}
-    end
-  end
-
-  # testing-only Client API
-
-  @doc """
-  close the to-do sever process and leaving the to-do list data on the disk
-  (remove todo-server-process from memory)
-  """
-  defp_testable close_process(todo_list_name) do
-    cache_pid = Process.whereis(__MODULE__)
-    GenServer.call(cache_pid, {:close_process, todo_list_name})
-  end
 
   @doc """
   terminate Cache and all it childrens(todo-servers)
